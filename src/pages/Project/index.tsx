54379cda83120be5f88e42e0908efb547af96ce1
Germain <AR0Z@protonmail.com>
2025-09-02 21:02:51 +0200

    fix: Update language fallback for project titles and types to prioritize French

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -24,23 +24,16 @@ export function Project({ id }) {
   }
 
   const imgSrc = images[`/src/assets/projects/${project.image}`] as string;
-  const langSSR = typeof window === "undefined" ? "fr" : lang;
-
-  const description =
-    project.description?.[langSSR] ||
-    project.description?.["fr"] ||
-    project.description?.["en"] ||
-    "Description manquante";
 
   return (
     <section className="project">
       <div className="project__title">
         <h1 className="project__title__text">
-          {project.title[lang] || project.title["en"]}
+          {project.title[lang] || project.title["fr"]}
         </h1>
         <img className="project__title__image" src={imgSrc} alt="" />
         <h2 className="project__title__subtitle">
-          {project.type[lang] || project.type["en"]}
+          {project.type[lang] || project.type["fr"]}
         </h2>
       </div>
       <div className="project__content">
@@ -83,7 +76,15 @@ export function Project({ id }) {
           </div>
         )}
 
-        <p dangerouslySetInnerHTML={{ __html: description }} />
+        <p
+          dangerouslySetInnerHTML={{
+            __html:
+              project.description[lang] ||
+              project.description["fr"] ||
+              project.description["en"] ||
+              "Description manquante",
+          }}
+        />
       </div>
     </section>
   );
